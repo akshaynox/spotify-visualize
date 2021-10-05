de79b36afdd5b63cb21587362963ddd220e5842d
Akshay <aksingh240@gmail.com>
2021-10-06 01:12:44 +0530

    Added utility functions for access & refresh tokens in localStorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,29 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { access_token, logout } from './spotify';
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState(null);
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const access_token = urlParams.get('access_token');
-    const refresh_token = urlParams.get('refresh_token');
-    console.log(access_token, refresh_token);
-
+    setToken(access_token);
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {!token ? (
         <a
           className="App-link"
           href="http://localhost:8080/login"
         >
           Login to Spotify
         </a>
+        ): (
+          <>
+            <h1>Logged in!</h1>
+            <button onClick={ logout }>Log Out</button>
+          </>
+        )}
       </header>
     </div>
   );
